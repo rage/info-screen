bd69c604f562686f5c6ff1f8899bec1d70447b2a
Kauhsa <kauhsa@kapsi.fi>
2014-09-22 21:54:32 +0300

    Major changes - has transitions now

---
+++ js/infoscreen.js
@@ -0,0 +1,105 @@
+var LIST_ID = "53f47cc6966bb91ba14291b7";
+
+var infoScreenApp = angular.module('infoScreenApp', []);
+
+infoScreenApp.service('TrelloService', function($rootScope) {
+	Trello.authorize();
+
+	return {
+		getCards: function(listId, success) {
+			Trello.get("lists/" + listId + "/cards", { members: true }).success(function(result) {
+				$rootScope.$apply(function() {
+					success(result);
+				});
+			});
+		}
+	};
+});
+
+infoScreenApp.controller('InfoScreenCtrl', function ($scope, $interval, TrelloService) {
+	$scope.doingCards = [];
+	$scope.moocCards = [];
+
+	function update() {
+		TrelloService.getCards("53f47cc6966bb91ba14291b7", function(res) {
+			$scope.doingCards = res;
+		});
+
+		TrelloService.getCards("53fa168af8454125a3d17e12", function(res) {
+			$scope.moocCards = res;
+		});
+	}
+
+	$interval(update, 1000 * 60 * 5);
+	update();
+});
+
+infoScreenApp.directive('trellocards', function() {
+	return {
+		restrict: 'E',
+	    scope: {
+	    	cards: '='
+	    },
+	    templateUrl: 'partials/trelloCards.html'
+	};
+});
+
+infoScreenApp.directive('transitionpages', function() {
+	return {
+		restrict: 'E',
+		scope: {
+			transitionInterval: '=transitionInterval'
+		},
+		transclude: true,
+		template: '<div ng-transclude></div>',
+		link: function(scope, element, attrs) {
+			element.addClass("pt-perspective");
+		},
+		controller: function($scope, $interval) {
+			var pages = [];
+			var currentPage = 0;
+
+			this.addPage = function(elem) {
+				pages.push(elem);
+			};
+
+			this.isPages = function() {
+				return pages.length !== 0;
+			};
+
+			function changeToNextPage() {
+				oldPage = currentPage;
+				currentPage = (currentPage + 1) % pages.length;
+
+				pages[currentPage].addClass("pt-page-current")
+					.addClass("pt-page-moveFromRight")
+					.removeClass("pt-page-moveToLeft");
+
+				pages[oldPage].addClass("pt-page-moveToLeft")
+					.removeClass("pt-page-moveFromRight");
+			}
+
+			timeoutId = $interval(function() {
+		        changeToNextPage();
+		    }, parseInt($scope.transitionInterval));
+		}
+	};
+});
+
+infoScreenApp.directive('transitionpage', function() {
+	function link(scope, element, attrs, pagesCtrl) {
+		if (!pagesCtrl.isPages()) {
+			element.addClass("pt-page-current");
+		}
+		element.addClass("pt-page");
+		pagesCtrl.addPage(element);
+	}
+
+	return {
+		require: '^transitionpages',
+		restrict: 'E',
+		transclude: true,
+		template: '<div ng-transclude></div>',
+		link: link
+	};
+});
