0ee2e1553eef20132e71be7d16cdd3e47143407b
Kauhsa <kauhsa@kapsi.fi>
2014-09-23 16:37:17 +0300

    Add clock

--- js/infoscreen.js
+++ js/infoscreen.js
@@ -62,6 +62,38 @@ infoScreenApp.directive('trellocards', function() {
     };
 });
 
+
+infoScreenApp.directive('currentTime', ['$interval', 'dateFilter', function($interval, dateFilter) {
+    function link(scope, element, attrs) {
+        var timeoutId;
+
+        function updateTime() {
+            element.text(dateFilter(new Date(), scope.format));
+        }
+
+        scope.$watch(attrs.myCurrentTime, function(value) {
+            format = value;
+            updateTime();
+        });
+
+        element.on('$destroy', function() {
+           $interval.cancel(timeoutId);
+        });
+
+        // start the UI update process; save the timeoutId for canceling
+        timeoutId = $interval(function() {
+            updateTime(); // update DOM
+        }, 1000);
+    }
+
+    return {
+        link: link,
+        scope: {
+            format: "@"
+        }
+    };
+}]);
+
 infoScreenApp.directive('transitionpages', function() {
     return {
         restrict: 'E',
